01c000f434a7c74746a1def8c1dfc87227c6f42e
Gulshan Kumar K <gulshanjitm@gmail.com>
2023-05-24 02:08:17 +0530

    Delete publish.bat

--- admin/publish.bat
+++
@@ -1,7 +0,0 @@
-nuget pack Proto.Service.Interface.Generator.nuspec
-nuget add Proto.Service.Interface.Generator.1.0.0-rc1.nupkg -Source "D:\WS\LocalNuget"
-nuget pack Proto.Service.ProtoEndPoint.Generator.nuspec
-nuget add Proto.Service.ProtoEndPoint.Generator.1.0.0-rc1.nupkg -Source "D:\WS\LocalNuget"
-nuget pack Proto.Service.AspNetController.Generator.nuspec
-nuget add Proto.Service.AspNetController.Generator.1.0.0-rc1.nupkg -Source "D:\WS\LocalNuget"
-pause
